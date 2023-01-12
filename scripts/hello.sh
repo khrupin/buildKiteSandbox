e59395fa9d2a7456a15a46d8c596815cfb5330a0
Igor Khrupin <igor@hrupin.com>
2023-01-12 10:44:29 -0800

    download artifact

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -1,2 +1,9 @@
-echo "Code Coverage Reported"
-echo $1
+#!/bin/bash
+echo "--- Retrieve Jacoco XML Report"
+buildkite-agent artifact download " **/mockBuildDir/reports/jacoco/*/*.xml" .
+
+echo "--- Parse Report"
+PATH_TO_XML_REPORT=$(find . -name *.xml)
+echo "PATH_TO_XML_REPORT: ${PATH_TO_XML_REPORT}"
+
+wc -c PATH_TO_XML_REPORT
