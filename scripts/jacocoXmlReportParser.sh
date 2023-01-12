050ceac538a16a965af687ecd4d5d75c534c0d0d
Igor Khrupin <igor@hrupin.com>
2023-01-12 11:21:05 -0800

    added bash script

--- scripts/jacocoXmlReportParser.sh
+++ scripts/jacocoXmlReportParser.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-xmlReportFile=$1
+echo "--- Retrieve Jacoco XML Report"
+buildkite-agent artifact download "buildKiteSandbox/mockBuildDir/reports/jacoco/*/*.xml" .
+
+echo "--- Parse Report"
+xmlReportFile=$(find . -name jacocoTestReport.xml)
+echo "PATH_TO_XML_REPORT: ${xmlReportFile}"
+
+wc -c xmlReportFile
 
 #missedNodesStr= doc.xpath("/report/counter[@type='INSTRUCTION']/@missed")
 missedNodesStr=$(xmllint --xpath "string(/report/counter[@type='INSTRUCTION']/@missed)" $xmlReportFile)
