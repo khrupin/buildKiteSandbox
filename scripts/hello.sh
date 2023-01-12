4ddefa5317b0ac8a81d78560a4055fb888d77705
Igor Khrupin <igor@hrupin.com>
2023-01-12 10:49:23 -0800

    ch path

--- scripts/hello.sh
+++ scripts/hello.sh
@@ -3,7 +3,7 @@ echo "--- Retrieve Jacoco XML Report"
 buildkite-agent artifact download "buildKiteSandbox/mockBuildDir/reports/jacoco/*/*.xml" .
 
 echo "--- Parse Report"
-PATH_TO_XML_REPORT=$(find . -name *.xml)
+PATH_TO_XML_REPORT=$(find . -name jacocoTestReport.xml)
 echo "PATH_TO_XML_REPORT: ${PATH_TO_XML_REPORT}"
 
 wc -c PATH_TO_XML_REPORT
